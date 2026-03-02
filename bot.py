9dfb33dbf023265a2c140338dced1d2f7d1c5dde
mustafo20111104 <ahrorbekabduqahharov688@gmail.com>
2026-03-02 20:04:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from telegram.ext import (
     filters,
 )
 
-TOKEN = "8312461995:AAGqJkT11O9zqoSrw3NP4D_1fbl6Wrg2xQk"
+TOKEN = "8312461995:AAExjPqVRhrHvhBQVi4XALAn-cNyM5RZsYw"
 YT_API_KEY = "AIzaSyCTHPm3oLBd-vXhl1JH9rEYOvbt1USOvzg"
 ADMIN_ID = "6705765282"
 
@@ -791,6 +791,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
